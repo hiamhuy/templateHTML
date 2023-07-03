de775edf03c9ac00ab49972cbd10a12ee6b0f866
hahuy2kbg <huyhue2kbg@gmail.com>
2023-07-03 17:38:15 +0700

    init

---
+++ blog.js
@@ -0,0 +1,28 @@
+const toggleBtn = document.querySelector(".btn-toggle");
+const closeBtn = document.querySelector(".btn-close");
+
+const menuMobile = document.querySelector("aside");
+const overlay = document.querySelector(".overlay");
+
+toggleBtn.addEventListener("click", () => {
+	overlay.classList.toggle("active-overlay");
+	menuMobile.classList.toggle("active");
+});
+closeBtn.addEventListener("click", () => {
+	overlay.classList.remove("active-overlay");
+	menuMobile.classList.remove("active");
+});
+
+//menu tabs
+const tabs = document.querySelectorAll(".tab");
+tabs.forEach((tab) => {
+	tab.addEventListener("click", activeTabs);
+});
+function activeTabs(e) {
+	e.preventDefault();
+	tabs.forEach((item) => {
+		item != this ? item.closest(".tab").classList.remove("active") : null;
+	});
+	if (this.closest(".tab").classList != "active");
+	this.closest(".tab").classList.toggle("active");
+}
